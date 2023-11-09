8c9128616c09c622c48c3b07cbeb39fbc6ae4780
Artur Ceschin <artur.ceschin@gmail.com>
2023-11-09 11:52:43 -0300

    feat: search update and PUT method

--- src/database.js
+++ src/database.js
@@ -36,6 +36,29 @@ export class Database {
     this.#persist()
   }
 
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if(search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value)
+        })
+      })
+    }
+
+    return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if(rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+
   delete(table, id) {
     const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
